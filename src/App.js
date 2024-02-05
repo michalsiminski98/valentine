1f5f96b0069090fd2b3b0d9bad1ea557cfe7b735
michal siminski <michal.siminski3@gmail.com>
2024-02-05 23:34:57 +0100

    add canvas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import './App.css';
+import confetti from "canvas-confetti";
 
 const windowWidth = window.innerWidth;
+
+const heart = confetti.shapeFromPath({
+  path: 'M167 72c19,-38 37,-56 75,-56 42,0 76,33 76,75 0,76 -76,151 -151,227 -76,-76 -151,-151 -151,-227 0,-42 33,-75 75,-75 38,0 57,18 76,56z',
+  matrix: [0.03333333333333333, 0, 0, 0.03333333333333333, -5.566666666666666, -5.533333333333333]
+});
+
+const canvasSettings = {
+  scalar: 4,
+  spread: 180,
+  particleCount: 260,
+  origin: { y: -0.1 },
+  startVelocity: -35
+};
+
+
 export default function Page() {
   const [noCount, setNoCount] = useState(0);
   const [yesPressed, setYesPressed] = useState(false);
@@ -29,12 +45,23 @@ export default function Page() {
     return `${process.env.PUBLIC_URL}/images/image${noCount < 6 ? noCount : 5}.jpg`;
   }
 
+  useEffect(() => {
+    if(yesPressed){
+      confetti({
+        ...canvasSettings,
+        shapes: [heart],
+        colors: ['#f93963', '#a10864', '#ee0b93']
+      });
+    }
+
+  },[yesPressed])
+
   return (
     <div className="wrapper" style={{ cursor: `url(${process.env.PUBLIC_URL}/images/cursor.png), auto `}}>
       {yesPressed ? (
         <>
         <img className="image" alt="us" src={`${process.env.PUBLIC_URL}/images/doge.png`} />
-        <div className="text-4xl font-bold my-4">yayyyy</div>
+        <h1>To do zobaczenia ; )</h1>
         <img className="gif" alt="gif"src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
         </>
       ) : (
